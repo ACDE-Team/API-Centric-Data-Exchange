9e613e0752832b8520a2b5557c405c8fbf63eff9
Eyosiyas Mekbib <eyosi9000@gmail.com>
2022-09-28 13:22:41 +0300

    ACDE-10 Added Link Extractor

--- Converter/services.py
+++ Converter/services.py
@@ -2,6 +2,17 @@ import xmltodict
 import json
 import requests
 
+def link_extractor(link):
+    code = link.split("/",5)[-1]
+    po_head_code = f"{code[1:9]}-{code[9:13]}-{code[13:17]}-{code[17:21]}-{code[21:]}"
+
+    po_header_link = f"https://test.mymxp.com/x/MXP_ECom_ISAPI.dll/MW/QuoteDetail/PurchaseOrderHead?_dc=1663656477193&firstrow=true&documentID=%7B{po_head_code}%7D&"
+    po_items_link = f"https://www.mymxp.com/cgi-bin/ExportDoc.exe/POInterfaceFile{code}&XML"
+
+    return [po_header_link, po_items_link]
+
+
+
 def xml_to_json(links):
     return_value = []
     for link in links:
